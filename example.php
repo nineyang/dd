49d6c7a0e1547be4967389e68ff8188a8edd7871
nine <nine@segmentfault.com>
2017-09-10 00:32:07 +0800

    readme

--- example.php
+++ example.php
@@ -8,15 +8,23 @@
 require 'vendor/autoload.php';
 
 //string
-//\dd\Dump::dump('helloworld');
+//\dd\Dump::dump('hello,nine');
 
 //array
 //\dd\Dump::dump(['hello', 'world' => ['aa' , 'bb' => ['cc' , 'dd']]]);
 
 //function
-\dd\Dump::dump(function ($name = 'nine' , Closure $closure) {
-    echo $name;
-});
+//\dd\Dump::dump(function ($name = 'nine' , Closure $closure) {
+//    echo $name;
+//});
 
 //object
 //\dd\Dump::dump(new stdClass());
+
+
+function dd($value)
+{
+    \dd\Dump::dump($value);
+}
+
+dd("hello,nine");
